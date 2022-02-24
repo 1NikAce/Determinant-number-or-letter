a878fffa2f3e59d254dff015b6be2c2f8a993a4c
_NikAce_ <lesnoi228@yandex.ru>
2022-02-24 16:51:12 +0300

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream> 
+#include <cctype> 
+#include <Windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "russia");
+    int count = 0;
+    char first[255];
+    cin >> first;
+
+    while (first[count])
+    {
+        if (isalpha(first[count]))
+            cout << "Символ " << first[count] << " - буква" << endl;
+        else
+            cout << "Символ " << first[count] << " - Цифра" << endl;
+        count++;
+    }
+    return 0;
+
+}
